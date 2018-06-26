1a0b193458e018e2c559cbe6f3d394ec66ff7885
Emily Jin <jin914@usc.edu>
2018-06-25 20:00:13 -0700

    ?

--- Day11.rb
+++ Day11.rb
@@ -15,7 +15,8 @@ max_sum = 0;
 for i in (0...arr.length-2)
     for j in (0...arr[i+2].length-2)
         curr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
-        if(curr_sum > max_sum)
+        puts curr_sum
+	if(curr_sum > max_sum)
             max_sum = curr_sum
         end
         curr_sum = 0
