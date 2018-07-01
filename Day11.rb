ddc2feedf2fd7275a195bd50f97af4da0393f8a3
Emily Jin <jin914@usc.edu>
2018-06-30 20:26:21 -0700

    fixed an edge case

--- Day11.rb
+++ Day11.rb
@@ -16,7 +16,7 @@ for i in (0...arr.length-2)
     for j in (0...arr[i+2].length-2)
         curr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
         puts curr_sum
-	if(curr_sum > max_sum)
+	if(curr_sum > max_sum || (j==0 && i==0))
             max_sum = curr_sum
         end
         curr_sum = 0
